59bad62f2d48dd2ff0c09bbbf8c4efa00509e3c6
Luigi Minardi <luigiminardim@gmail.com>
2023-02-21 15:51:03 -0300

    feat: :sparkles: https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+/**
+ * This is a simple guessing game.
+ * The program will generate a random number between 1 and 100.
+ * The user will then be prompted to guess the number.
+ * The program will then tell the user if their guess is too high or too low.
+ * If the user guesses the correct number, the program will print a congratulatory message.
+ */
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Please input your guess.");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
+        let result = guess.cmp(&secret_number);
+        match result {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
