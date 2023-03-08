ece4dab5963e65d55528f310d0ff2c94f04f5364
Luigi Minardi <luigiminardim@gmail.com>
2023-03-08 13:45:09 -0300

    feat: :sparkles: https://doc.rust-lang.org/book/ch12-00-an-io-project.html

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::{env, process};
+
+use minigrep::{run, Config};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for \"{}\"", config.query);
+    println!("In file {}", config.file_path);
+    if let Err(err) = run(config) {
+        eprintln!("Application error: {}", err);
+        process::exit(1);
+    };
+}
